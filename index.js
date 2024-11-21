c7c25158f586f191270c9776682c0c2a3c7d5f11
Tohibur Rahman Tauhid <tohibur.tauhid@gmail.com>
2024-11-21 19:50:15 +0530

    Added email and fixed bugs

--- index.js
+++ index.js
@@ -18,17 +18,13 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lhuqf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
-
-const client = new MongoClient(
-  uri,
-  {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   }
-);
+});
 
 async function run() {
   try {
--- readme.md
+++ readme.md
@@ -8,5 +8,18 @@ The middleware or packages I have used in this project are:
 
 
 
+<!-- The react hooks I have used here. -->
+
+1. useEffect
+2. useState
+3. useContext
+4. useReducer
+5. useCallback
+6. useMemo
+7. customHooks
+
+I also have used the concept of private router for the access of admin and users separately.
+
+
 <!-- Problem couldn't solve -->
 Mongodb connection failure
